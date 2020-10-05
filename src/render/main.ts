0fa706437d0bae66a1026607a075dc2c4a835bda
草鞋没号 <308487730@qq.com>
2020-10-05 21:34:46 +0800

    add electron-is-dev

--- src/render/main.ts
+++ src/render/main.ts
@@ -2,11 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // vite 会编译 import 的形式；所以 electron 及 node.js 内置模块用 require 形式
 const { ipcRenderer } = require('electron')
-import store from '/utils/store'
+import { store, isdev } from '/utils/index'
 
 import './index.css'
 
 console.log('ipcRenderer:', ipcRenderer)
 console.log('Store', store)
+console.log('electron is dev', isdev)
 
 createApp(App as any).mount('#app')
---
+++ src/render/utils/index.ts
@@ -0,0 +1,7 @@
+import store from './store'
+const isdev = require('electron-is-dev')
+
+export {
+  store,
+  isdev,
+}
