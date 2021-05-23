e7406e9c13432f4335a9bd3cdc989756fa10fe80
草鞋没号 <308487730@qq.com>
2021-05-23 10:35:34 +0800

    feat: use vitejs-plugin-electron

--- vite.config.ts
+++ vite.config.ts
@@ -3,32 +3,19 @@ require('dotenv').config({ path: join(__dirname, '.env') })
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { join } from 'path'
-// import typescript from '@rollup/plugin-typescript'
 import { builtins } from './script/utils'
-import {
-  // ensureCwdCrrect,
-  esm2cjs,
-} from './script/plugins'
+import electron from 'vitejs-plugin-electron'
 
 const root = join(__dirname, 'src/render')
 
-// https://vitejs.dev/config/
 export default defineConfig(env => {
   const isserve = env.command === 'serve'
 
   return {
     plugins: [
       vue(),
-      ...(isserve ? [
-        /**
-         * !!! 开发期将 electron 及相关模块转换成 commonjs 避开 vite 处理
-         */
-        esm2cjs([
-          'electron',
-          'electron-store',
-        ]),
-      ] : []),
-    ],
+      isserve && electron({}),
+    ].filter(Boolean),
     root,
     base: './', // index.html 中静态资源加载位置
     server: {
@@ -45,7 +32,6 @@ export default defineConfig(env => {
     optimizeDeps: {
       exclude: [
         'electron',
-        // 'electron-store', // optional
       ],
     },
     build: {
@@ -56,10 +42,6 @@ export default defineConfig(env => {
       assetsDir: '', // 相对路径 加载问题
       sourcemap: true,
       rollupOptions: {
-        plugins: [
-          // typescript({ module: 'ESNext' }),
-          // ensureCwdCrrect(join(root, 'main.ts')),
-        ],
         external: [
           ...builtins(),
           'electron',
