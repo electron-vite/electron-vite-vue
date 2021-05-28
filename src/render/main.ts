46d0b851703c43e780fb67df2d33140c8255eb60
草鞋没号 <308487730@qq.com>
2021-05-28 18:14:42 +0800

    chore: electron-store 传递 name 后影响热更新

--- src/render/main.ts
+++ src/render/main.ts
@@ -7,7 +7,10 @@ import Store from 'electron-store'
 import './index.css'
 
 console.log('ipcRenderer:', ipcRenderer)
-console.log('electron-store', new Store({ name: 'electron-vue' }))
+// Configuration name causes hot updates to be slow | 传递 name 后会导致热更新很慢
+// console.log('electron-store', new Store({ name: 'electron-vue' })) 
+// https://github.com/caoxiemeihao/electron-vue-vite/issues/10
+console.log('electron-store', new Store())
 
 createApp(App)
   .mount('#app')
