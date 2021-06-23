0f390defd56f8efd05ac81c4f025b1f85015050d
草鞋没号 <308487730@qq.com>
2021-06-23 10:58:52 +0800

    chore: redeclare typings

---
+++ typings/assets.d.ts
@@ -0,0 +1,24 @@
+declare module '*.png' {
+  const src: string
+  export default src
+}
+
+declare module '*.jpg' {
+  const src: string
+  export default src
+}
+
+declare module '*.gif' {
+  const src: string
+  export default src
+}
+
+declare module '*.webp' {
+  const src: string
+  export default src
+}
+
+declare module '*.svg' {
+  const src: string
+  export default src
+}
