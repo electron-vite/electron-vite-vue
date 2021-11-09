9eda22f3534274e35cdc95f2600d8e32248b260f
草鞋没号 <308487730@qq.com>
2021-11-09 09:01:52 +0800

    feat: better hot reload preload script

--- scripts/build-preload.ts
+++ scripts/build-preload.ts
@@ -1,36 +1,43 @@
-import { watch, rollup, OutputOptions } from 'rollup'
+import { watch, rollup, OutputOptions, Plugin } from 'rollup'
 import minimist from 'minimist'
 import chalk from 'chalk'
 import ora from 'ora'
 import WebSocket from 'ws'
 import options from './rollup.config'
-import { createWsServer, formatWsSendData } from './ws'
+import { createWsServer, formatWsSendData, WssServer } from './ws'
 
 const argv = minimist(process.argv.slice(2))
-const opt = options({ proc: 'preload', env: argv.env })
+const opts = options({ proc: 'preload', env: argv.env })
 const TAG = '[build-preload.ts]'
 const spinner = ora(`${TAG} Electron preload build...`)
 
+function hotReloadPreload(wssServer: WssServer): Plugin {
+  return {
+    name: 'hot-reload-preload',
+    writeBundle() {
+      // Hot reload preload script !!!
+      if (wssServer.instance?.readyState === WebSocket.OPEN) {
+        console.log(TAG, 'Hot reload preload script')
+        wssServer.instance.send(formatWsSendData({ cmd: 'reload', data: Date.now() }))
+      }
+    },
+  }
+}
+
 ; (async () => {
   if (argv.watch) {
-    const watcher = watch(opt)
-    const wssObj = createWsServer({ TAG })
+    const wssServer = createWsServer()
+    opts.plugins = opts.plugins?.concat(hotReloadPreload(wssServer))
+    const watcher = watch(opts)
 
     watcher.on('change', filename => {
-      const log = chalk.yellow(`change -- ${filename}`)
-      console.log(TAG, log)
-
-      // Hot reload renderer process !!!
-      if (wssObj.instance?.readyState === WebSocket.OPEN) {
-        console.log(TAG, 'Hot reload renderer process')
-        wssObj.instance.send(formatWsSendData({ cmd: 'reload', data: Date.now() }))
-      }
+      console.log(TAG, chalk.yellow(`change -- ${filename}`))
     })
   } else {
     spinner.start()
     try {
-      const build = await rollup(opt)
-      await build.write(opt.output as OutputOptions)
+      const build = await rollup(opts)
+      await build.write(opts.output as OutputOptions)
       spinner.succeed()
       process.exit()
     } catch (error) {
