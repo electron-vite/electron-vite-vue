321ff2ddd700d9a207f4ed610844c8ba0a5c65d5
草鞋没号 <308487730@qq.com>
2021-12-14 16:16:43 +0800

    electron-builder use npm command

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,8 +1,6 @@
 process.env.NODE_ENV = 'production'
 
 import { build as viteBuild } from 'vite'
-import { build as electronBuild, Platform } from 'electron-builder'
-import { config as builderConfig } from '../configs/electron-builder.config.mjs'
 import chalk from 'chalk'
 
 const TAG = chalk.bgBlue(' build.mjs ')
@@ -22,16 +20,5 @@ async function buildElectron() {
   }
 }
 
-async function packElectron() {
-  return electronBuild({
-    config: builderConfig,
-    // if you want to build windows platform
-    // targets: Platform.WINDOWS.createTarget(),
-  }).then(result => {
-    console.log(TAG, 'files:', chalk.green(result))
-  })
-}
-
 // bootstrap
 await buildElectron()
-await packElectron()
