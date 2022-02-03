d6e18ecae4a4d1b37db4569251b5aab683a23cd5
草鞋没号 <308487730@qq.com>
2022-02-04 07:59:13 +0800

    refactor: optimize code

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -6,35 +6,20 @@ import { build } from 'vite'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-/**
- * @type {Record<string, import('vite').InlineConfig>}
- */
-const viteConfigs = {
-  main: {
-    configFile: 'scripts/vite.config.mjs',
-    root: join(__dirname, '../src/main'),
-    build: {
-      outDir: '../../dist/main',
-    },
+await build({
+  configFile: 'scripts/vite.config.mjs',
+  root: join(__dirname, '../src/main'),
+  build: {
+    outDir: '../../dist/main',
   },
-  preload: {
-    configFile: 'scripts/vite.config.mjs',
-    root: join(__dirname, '../src/preload'),
-    build: {
-      outDir: '../../dist/preload',
-    },
-  },
-  renderer: {
-    configFile: 'src/renderer/vite.config.ts',
-  },
-}
+})
 
-async function buildElectron() {
-  for (const [, config] of Object.entries(viteConfigs)) {
-    await build(config)
-    console.log() // for beautiful log.
-  }
-}
+await build({
+  configFile: 'scripts/vite.config.mjs',
+  root: join(__dirname, '../src/preload'),
+  build: {
+    outDir: '../../dist/preload',
+  },
+})
 
-// bootstrap
-await buildElectron()
+await build({ configFile: 'src/renderer/vite.config.ts' })
