f95f77aa6290517bd99f1f1c25179e89138c90b5
草鞋没号 <308487730@qq.com>
2022-02-08 11:24:32 +0800

    format code

--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -52,34 +52,34 @@ export function resolveElectron(
 
   function electronExport() {
     return `
-  /**
-   * For all exported modules see https://www.electronjs.org/docs/latest/api/clipboard -> Renderer Process Modules
-   */
-  const electron = require("electron");
-  const {
-    clipboard,
-    nativeImage,
-    shell,
-    contextBridge,
-    crashReporter,
-    ipcRenderer,
-    webFrame,
-    desktopCapturer,
-    deprecate,
-  } = electron;
+/**
+ * For all exported modules see https://www.electronjs.org/docs/latest/api/clipboard -> Renderer Process Modules
+ */
+const electron = require("electron");
+const {
+  clipboard,
+  nativeImage,
+  shell,
+  contextBridge,
+  crashReporter,
+  ipcRenderer,
+  webFrame,
+  desktopCapturer,
+  deprecate,
+} = electron;
 
-  export {
-    electron as default,
-    clipboard,
-    nativeImage,
-    shell,
-    contextBridge,
-    crashReporter,
-    ipcRenderer,
-    webFrame,
-    desktopCapturer,
-    deprecate,
-  }
+export {
+  electron as default,
+  clipboard,
+  nativeImage,
+  shell,
+  contextBridge,
+  crashReporter,
+  ipcRenderer,
+  webFrame,
+  desktopCapturer,
+  deprecate,
+}
 `
   }
 
