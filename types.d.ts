6e184ad54c06faef0d5c921e0831488313e6be1e
草鞋没号 <308487730@qq.com>
2022-02-08 11:13:34 +0800

    rename

--- types.d.ts
+++ types.d.ts
@@ -2,7 +2,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production'
-    readonly PORT: string
-    readonly HOST: string
+    readonly VITE_DEV_SERVER_HOST: string
+    readonly VITE_DEV_SERVER_PORT: string
   }
 }
