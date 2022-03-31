4d9849a7182a97000f6a3a73789be950c2dcbdf9
zhoufeifan <937127883@qq.com>
2022-03-31 14:57:00 +0800

    Update index.ts
    去掉额外的分号

--- packages/preload/index.ts
+++ packages/preload/index.ts
@@ -2,6 +2,6 @@ import { domReady } from './utils'
 import { useLoading } from './loading'
 
 const { appendLoading, removeLoading } = useLoading()
-window.removeLoading = removeLoading;
+window.removeLoading = removeLoading
 
 domReady().then(appendLoading)
