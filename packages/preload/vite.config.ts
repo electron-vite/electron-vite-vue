0ac3c660877cf1bcda7be230991651bb28f7f5cb
草鞋没号 <308487730@qq.com>
2022-04-15 07:35:20 +0800

    feat: multiple preload entry

--- packages/preload/vite.config.ts
+++ packages/preload/vite.config.ts
@@ -1,3 +1,4 @@
+import { join } from 'path'
 import { builtinModules } from 'module'
 import { defineConfig } from 'vite'
 import pkg from '../../package.json'
@@ -6,15 +7,19 @@ export default defineConfig({
   root: __dirname,
   build: {
     outDir: '../../dist/preload',
-    lib: {
-      entry: 'index.ts',
-      formats: ['cjs'],
-      fileName: () => '[name].cjs',
-    },
     minify: process.env./* from mode option */NODE_ENV === 'production',
     // https://github.com/caoxiemeihao/electron-vue-vite/issues/61
     sourcemap: 'inline',
     rollupOptions: {
+      input: {
+        // multiple entry
+        index: join(__dirname, 'index.ts'),
+      },
+      output: {
+        format: 'cjs',
+        entryFileNames: '[name].cjs',
+        manualChunks: {},
+      },
       external: [
         'electron',
         ...builtinModules,
