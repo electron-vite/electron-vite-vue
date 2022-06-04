a37a30e18a40f8cad923df625659c24979c92e80
草鞋没号 <308487730@qq.com>
2022-06-05 00:13:02 +0800

    feat: remove dist before run build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
+import { rmSync } from 'fs'
 import { join } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import electron from 'vite-plugin-electron'
 import renderer from 'vite-plugin-electron/renderer'
+
+rmSync('dist', { recursive: true, force: true }) // v14.14.0
 
 // https://vitejs.dev/config/
 export default defineConfig({
