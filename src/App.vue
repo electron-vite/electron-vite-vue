4d830c60cd6a7d1a0471f0c4f72ad4ea0aac0d73
草鞋没号 <308487730@qq.com>
2022-06-04 10:29:50 +0800

    feat: Electron Demo

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,18 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="logo-box">
+    <img style="height:140px;" src="./assets/electron.png" >
+    <span/>
+    <img style="height:140px;" src="./assets/vite.svg" >
+    <span/>
+    <img style="height:140px;" src="./assets/vue.png" >
+  </div>
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="static-public">
+    Place static files into the <code>/public</code> folder
+    <img style="width:77px;" :src="'./node.png'" >
+  </div>
 </template>
 
 <style>
@@ -17,5 +27,29 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo-box {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+}
+
+.logo-box span {
+  width: 74px;
+}
+
+.static-public {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.static-public code {
+  background-color: #eee;
+  padding: 2px 4px;
+  margin: 0 4px;
+  border-radius: 4px;
+  color: #304455;
 }
 </style>
