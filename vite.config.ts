cda72bab8d8e9163e7411fb2972763dddd3e11c7
西瓜° <8441078+i_melon@user.noreply.gitee.com>
2022-06-07 19:38:33 +0800

    add note

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,42 @@
-import { rmSync } from "fs";
-import { join } from "path";
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import electron from "vite-plugin-electron";
-import renderer from "vite-plugin-electron/renderer";
+import { rmSync } from 'fs';
+import { join } from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import electron from 'vite-plugin-electron';
+import renderer from 'vite-plugin-electron/renderer';
 
-rmSync("dist", { recursive: true, force: true }); // v14.14.0
+rmSync('dist', { recursive: true, force: true }); // v14.14.0
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig((env) => ({
   plugins: [
     vue(),
-    electron({
-      main: {
-        entry: "electron-main/index.ts",
-        vite: {
-          build: {
-            sourcemap: false,
+    env.mode != 'debug' &&
+      electron({
+        main: {
+          entry: 'electron-main/index.ts',
+          vite: {
+            build: {
+              sourcemap: false,
+            },
           },
         },
-      },
-      preload: {
-        input: {
-          // You can configure multiple preload here
-          splash: "electron-preload/splash.ts",
-        },
-        vite: {
-          build: {
-            // For debug
-            sourcemap: "inline",
+        preload: {
+          input: {
+            // You can configure multiple preload here
+            // name : path e.g.:index:'preload/index.ts'
+            // name should match with path in used electron main -> BrowserWindow ->preload
+            splash: 'electron-preload/splash.ts',
+          },
+          vite: {
+            build: {
+              // For debug
+              sourcemap: 'inline',
+            },
           },
         },
-      },
-    }),
+      }),
     // Enable use Electron, Node.js API in Renderer-process
     renderer(),
   ],
-});
+}));
