39c8e5fdd368af490d59450299b8e6c9426a42d4
西瓜° <8441078+i_melon@user.noreply.gitee.com>
2022-06-07 16:26:28 +0800

    fix dir error

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { rmSync } from 'fs'
-import { join } from 'path'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import electron from 'vite-plugin-electron'
-import renderer from 'vite-plugin-electron/renderer'
+import { rmSync } from "fs";
+import { join } from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import electron from "vite-plugin-electron";
+import renderer from "vite-plugin-electron/renderer";
 
-rmSync('dist', { recursive: true, force: true }) // v14.14.0
+rmSync("dist", { recursive: true, force: true }); // v14.14.0
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +13,7 @@ export default defineConfig({
     vue(),
     electron({
       main: {
-        entry: 'electron-main/index.ts',
+        entry: "electron-main/index.ts",
         vite: {
           build: {
             sourcemap: false,
@@ -23,17 +23,17 @@ export default defineConfig({
       preload: {
         input: {
           // You can configure multiple preload here
-          splash: join(__dirname, 'electron-preload/splash.ts'),
+          splash: "electron-preload/splash.ts",
         },
         vite: {
           build: {
             // For debug
-            sourcemap: 'inline',
-          }
+            sourcemap: "inline",
+          },
         },
       },
     }),
     // Enable use Electron, Node.js API in Renderer-process
     renderer(),
   ],
-})
+});
