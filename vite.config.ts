59a53b3ab8daa7573d3f1aeea1ea070486d7c7e5
草鞋没号 <308487730@qq.com>
2022-06-16 19:23:43 +0800

    refactor: directory structure

--- vite.config.ts
+++ vite.config.ts
@@ -13,22 +13,24 @@ export default defineConfig({
     vue(),
     electron({
       main: {
-        entry: 'electron-main/index.ts',
+        entry: 'electron/main/index.ts',
         vite: {
           build: {
             sourcemap: false,
+            outDir: 'dist/electron/main',
           },
         },
       },
       preload: {
         input: {
           // You can configure multiple preload here
-          splash: join(__dirname, 'electron-preload/splash.ts'),
+          splash: join(__dirname, 'electron/preload/splash.ts'),
         },
         vite: {
           build: {
             // For debug
             sourcemap: 'inline',
+            outDir: 'dist/electron/preload',
           }
         }
       },
