2dffca3b3249e026c6c3e11c84ac98933c4dae3a
草鞋没号 <308487730@qq.com>
2022-06-27 10:26:32 +0800

    feat: update config of vite-plugin-electron

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { rmSync } from 'fs'
+import { join } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import electron from 'vite-plugin-electron'
-import renderer from 'vite-plugin-electron/renderer'
-import electronConfig, { useNodeJsInElectronRenderer } from './vite.config.electron'
 
 rmSync('dist', { recursive: true, force: true }) // v14.14.0
 
@@ -11,9 +10,31 @@ rmSync('dist', { recursive: true, force: true }) // v14.14.0
 export default defineConfig({
   plugins: [
     vue(),
-    electron(electronConfig),
-    // Enable use Electron, Node.js API in Renderer-process
-    renderer(),
-    useNodeJsInElectronRenderer(),
+    electron({
+      main: {
+        entry: 'electron/main/index.ts',
+        vite: {
+          build: {
+            sourcemap: false,
+            outDir: 'dist/electron/main',
+          },
+        },
+      },
+      preload: {
+        input: {
+          // You can configure multiple preload here
+          splash: join(__dirname, 'electron/preload/splash.ts'),
+        },
+        vite: {
+          build: {
+            // For debug
+            sourcemap: 'inline',
+            outDir: 'dist/electron/preload',
+          }
+        }
+      },
+      // Enables use of Node.js API in the Renderer-process
+      renderer: {},
+    }),
   ],
 })
