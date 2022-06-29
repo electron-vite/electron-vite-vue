ef7022696b3c1b703118c053c57c76864a70c430
草鞋没号 <308487730@qq.com>
2022-06-29 08:36:32 +0800

    chore: remove comments

--- src/samples/node-api.ts
+++ src/samples/node-api.ts
@@ -2,7 +2,6 @@ import { lstat } from 'fs/promises'
 import { cwd } from 'process'
 import { ipcRenderer } from 'electron'
 
-// Usage of ipcRenderer.on
 ipcRenderer.on('main-process-message', (_event, ...args) => {
   console.log('[Receive Main-process message]:', ...args)
 })
