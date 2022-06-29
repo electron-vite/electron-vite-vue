67cf1e7f318fff092fd6812e5727479ef5734104
草鞋没号 <308487730@qq.com>
2022-06-29 08:36:58 +0800

    chore: use `postMessage` instead of `window.removeLoading`

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './samples/node-api'
+// import './samples/node-api'
 
 createApp(App)
   .mount('#app')
-  .$nextTick(window.removeLoading)
+  .$nextTick(() => {
+    postMessage({ payload: 'removeLoading' }, '*')
+  })
