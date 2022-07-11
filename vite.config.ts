158131d5dcacbf97730492530503e3661af738e6
Peyton Creery <44987569+phcreery@users.noreply.github.com>
2022-07-11 13:07:54 +0000

    correct dist folder handling

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import vue from '@vitejs/plugin-vue'
 import electron from 'vite-plugin-electron'
 import pkg from './package.json'
 
-rmSync('dist', { recursive: true, force: true }) // v14.14.0
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -40,5 +38,8 @@ export default defineConfig({
   server: {
     host: pkg.env.VITE_DEV_SERVER_HOST,
     port: pkg.env.VITE_DEV_SERVER_PORT,
+  },
+  build: {
+    emptyOutDir: true,
   },
 })
