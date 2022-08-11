7b27d3546b101fd78b7586ae33e2bb770b5a4d42
草鞋没号 <308487730@qq.com>
2022-08-11 09:25:19 +0800

    docs: `renderer` comments

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
         },
       },
       // Enables use of Node.js API in the Renderer-process
+      // https://github.com/electron-vite/vite-plugin-electron/tree/main/packages/electron-renderer#electron-renderervite-serve
       renderer: {},
     }),
     renderBuiltUrl(),
