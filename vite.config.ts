b4678fe50c260d15bed9ca4044886144898cec10
younglei <youngleish@gmail.com>
2022-08-11 10:50:30 +0800

    chore: remove renderBuiltUrl plugin

--- vite.config.ts
+++ vite.config.ts
@@ -40,7 +40,6 @@ export default defineConfig({
       // Enables use of Node.js API in the Renderer-process
       renderer: {},
     }),
-    renderBuiltUrl(),
   ],
   server: {
     host: pkg.env.VITE_DEV_SERVER_HOST,
@@ -62,60 +61,4 @@ function withDebug(config: UserConfig): UserConfig {
     })
   }
   return config
-}
-
-// Only worked Vite@3.x #212
-function renderBuiltUrl(): Plugin {
-  // https://github.com/vitejs/vite/blob/main/packages/vite/src/node/constants.ts#L84-L124
-  const KNOWN_ASSET_TYPES = [
-    // images
-    'png',
-    'jpe?g',
-    'jfif',
-    'pjpeg',
-    'pjp',
-    'gif',
-    'svg',
-    'ico',
-    'webp',
-    'avif',
-
-    // media
-    'mp4',
-    'webm',
-    'ogg',
-    'mp3',
-    'wav',
-    'flac',
-    'aac',
-
-    // fonts
-    'woff2?',
-    'eot',
-    'ttf',
-    'otf',
-
-    // other
-    'webmanifest',
-    'pdf',
-    'txt'
-  ]
-
-  return {
-    name: 'render-built-url',
-    config(config) {
-      config.experimental = {
-        renderBuiltUrl(filename, type) {
-          if (
-            KNOWN_ASSET_TYPES.includes(path.extname(filename).slice(1)) &&
-            type.hostType === 'js'
-          ) {
-            // Avoid Vite relative-path assets handling
-            // https://github.com/vitejs/vite/blob/89dd31cfe228caee358f4032b31fdf943599c842/packages/vite/src/node/build.ts#L838-L875
-            return { runtime: JSON.stringify(filename) }
-          }
-        },
-      }
-    },
-  }
 }
