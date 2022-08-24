642403f47827b8ca426f31054e5dc09f472a4acc
younglei <youngleish@gmail.com>
2022-08-24 21:00:38 +0800

    refactor: better Debug config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { rmSync } from 'fs'
 import path from 'path'
-import {
-  type Plugin,
-  type UserConfig,
-  defineConfig,
-} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import electron from 'vite-plugin-electron'
+import electron, { onstart } from 'vite-plugin-electron'
 import pkg from './package.json'
 
 rmSync('dist', { recursive: true, force: true }) // v14.14.0
@@ -18,11 +14,15 @@ export default defineConfig({
     electron({
       main: {
         entry: 'electron/main/index.ts',
-        vite: withDebug({
+        vite: {
           build: {
+            // For Debug
+            sourcemap: true,
             outDir: 'dist/electron/main',
           },
-        }),
+          // Will start Electron via VSCode Debug
+          plugins: [process.env.VSCODE_DEBUG ? onstart() : null],
+        },
       },
       preload: {
         input: {
@@ -47,19 +47,3 @@ export default defineConfig({
     port: pkg.env.VITE_DEV_SERVER_PORT,
   },
 })
-
-function withDebug(config: UserConfig): UserConfig {
-  if (process.env.VSCODE_DEBUG) {
-    if (!config.build) config.build = {}
-    config.build.sourcemap = true
-    config.plugins = (config.plugins || []).concat({
-      name: 'electron-vite-debug',
-      configResolved(config) {
-        const index = config.plugins.findIndex(p => p.name === 'electron-main-watcher');
-        // At present, Vite can only modify plugins in configResolved hook.
-        (config.plugins as Plugin[]).splice(index, 1)
-      },
-    })
-  }
-  return config
-}
