4b466018791c707e787c2c1bd03df3db43f35d47
草鞋没号 <308487730@qq.com>
2022-09-14 08:22:17 +0800

    update types

---
+++ electron/electron-env.d.ts
@@ -0,0 +1,24 @@
+/// <reference types="vite-plugin-electron/electron-env" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    /**
+     * The built directory structure
+     *
+     * ```tree
+     * ├─┬ dist
+     * │ ├─┬ electron
+     * │ │ ├─┬ main
+     * │ │ │ └── index.js
+     * │ │ └─┬ preload
+     * │ │   └── index.js
+     * │ ├── index.html
+     * │ ├── ...other-static-files-from-public
+     * │
+     * ```
+     */
+    DIST: string
+    /** /dist/ or /public/ */
+    PUBLIC: string
+  }
+}
