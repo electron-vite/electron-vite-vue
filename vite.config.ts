e522b5bbc63bb6b416e46b560ab4b5b205cb97be
草鞋没号 <308487730@qq.com>
2022-12-08 09:24:46 +0800

    feat: load `.env` file for Electron-Main

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { rmSync } from 'fs'
-import { defineConfig } from 'vite'
+import {
+  type Plugin,
+  defineConfig,
+  loadEnv,
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import electron from 'vite-plugin-electron'
 import renderer from 'vite-plugin-electron-renderer'
@@ -8,6 +12,25 @@ import pkg from './package.json'
 rmSync('dist-electron', { recursive: true, force: true })
 const sourcemap = !!process.env.VSCODE_DEBUG
 const isBuild = process.argv.slice(2).includes('build')
+
+// Load .env
+function loadEnvPlugin(): Plugin {
+  return {
+    name: 'vite-plugin-load-env',
+    config(config, env) {
+      const root = config.root ?? process.cwd()
+      const result = loadEnv(env.mode, root)
+      // Remove the vite-plugin-electron injected env.
+      delete result.VITE_DEV_SERVER_URL
+      config.esbuild ??= {}
+      config.esbuild.define = {
+        ...config.esbuild.define,
+        ...Object.fromEntries(Object.entries(result)
+          .map(([key, val]) => [`process.env.${key}`, JSON.stringify(val)])),
+      }
+    },
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -33,6 +56,7 @@ export default defineConfig({
               external: Object.keys(pkg.dependencies),
             },
           },
+          plugins: [loadEnvPlugin()],
         },
       },
       {
