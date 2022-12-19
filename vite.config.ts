7f003bd95ece3e2c819bcdab4701263255f4dcee
草鞋没号 <308487730@qq.com>
2022-12-19 09:53:10 +0800

    chore: remove `loadEnvPlugin()`

--- vite.config.ts
+++ vite.config.ts
@@ -13,25 +13,6 @@ rmSync('dist-electron', { recursive: true, force: true })
 const sourcemap = !!process.env.VSCODE_DEBUG
 const isBuild = process.argv.slice(2).includes('build')
 
-// Load .env
-function loadEnvPlugin(): Plugin {
-  return {
-    name: 'vite-plugin-load-env',
-    config(config, env) {
-      const root = config.root ?? process.cwd()
-      const result = loadEnv(env.mode, root)
-      // Remove the vite-plugin-electron injected env.
-      delete result.VITE_DEV_SERVER_URL
-      config.esbuild ??= {}
-      config.esbuild.define = {
-        ...config.esbuild.define,
-        ...Object.fromEntries(Object.entries(result)
-          .map(([key, val]) => [`process.env.${key}`, JSON.stringify(val)])),
-      }
-    },
-  }
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -56,7 +37,6 @@ export default defineConfig({
               external: Object.keys(pkg.dependencies),
             },
           },
-          plugins: [loadEnvPlugin()],
         },
       },
       {
