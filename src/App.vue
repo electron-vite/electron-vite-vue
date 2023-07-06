76846513dbdeb3b8808753b085400e2d8bf38850
草鞋没号 <308487730@qq.com>
2023-07-06 12:52:36 +0800

    chore: remove log

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-
-console.log("[App.vue]", `Hello world from Electron ${process.versions.electron}!`)
 </script>
 
 <template>
