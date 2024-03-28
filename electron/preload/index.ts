59d20f9095890baca86148671635467195bd93e4
badspider7 <1742969779@qq.com>
2024-03-28 13:00:46 +0800

    fix: resolve ipcRenderer  no returns

--- electron/preload/index.ts
+++ electron/preload/index.ts
@@ -4,19 +4,19 @@ import { ipcRenderer, contextBridge } from 'electron'
 contextBridge.exposeInMainWorld('ipcRenderer', {
   on(...args: Parameters<typeof ipcRenderer.on>) {
     const [channel, listener] = args
-    ipcRenderer.on(channel, (event, ...args) => listener(event, ...args))
+    return ipcRenderer.on(channel, (event, ...args) => listener(event, ...args))
   },
   off(...args: Parameters<typeof ipcRenderer.off>) {
     const [channel, ...omit] = args
-    ipcRenderer.off(channel, ...omit)
+    return ipcRenderer.off(channel, ...omit)
   },
   send(...args: Parameters<typeof ipcRenderer.send>) {
     const [channel, ...omit] = args
-    ipcRenderer.send(channel, ...omit)
+    return ipcRenderer.send(channel, ...omit)
   },
   invoke(...args: Parameters<typeof ipcRenderer.invoke>) {
     const [channel, ...omit] = args
-    ipcRenderer.invoke(channel, ...omit)
+    return ipcRenderer.invoke(channel, ...omit)
   },
 
   // You can expose other APTs you need here.
